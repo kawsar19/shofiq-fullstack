6945b1e085a973287c0bc2b2f88dc227a7ec504a
kawsar ahmed <kawsar.qtec@gmail.com>
2024-01-22 07:00:49 +0600

    first commit

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// MongoDB Connection
+mongoose.connect(
+  "mongodb+srv://kawsarbinjahangir:mWIlmJjfHvxBTJpC@cluster0.ngiowav.mongodb.net/school",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+// Define Madrasa Schema and Model (Replace 'path-to-your-madrasa-model' with the actual path)
+
+// Require Madrasa Routes (Replace 'path-to-your-madrasa-routes' with the actual path)
+const authRoutes = require("./routes/authController");
+
+// Use Madrasa Routes
+app.use("/api/auth/", authRoutes);
+
+app.get("/test", (req, res) => {
+  res.send("This is a test endpoint!");
+});
+// Start Server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
